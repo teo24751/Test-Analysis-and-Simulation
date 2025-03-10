0be7d957a4b1aa41d386e5b8d8e3c424ff8b0e42
Daniël <danielbezaan@gmail.com>
2025-03-10 11:33:28 +0100

    23ew

--- DeterminingCompliance.py
+++ DeterminingCompliance.py
@@ -1,13 +1,24 @@
 import numpy as np
 import math
+import readData as rd
+
+array = rd.load_displacement_curve(3)
+
 arr = [[0.001,1],[0.002,3],[0.003,3.5],[0.004,4], [0.005,6]]
 arr = np.array(arr)
 
+#for i in range(4):
+    #print(array[i+53])
 
-def compliance(n):
-    angle = (arr[n+1][0]-arr[n][0]) / (arr[n+1][1]-arr[n][1])
+
+def compliance(n,arr):
+    angle = (arr[n+2][0]-arr[n][0]) / (arr[n+2][1]-arr[n][1])
     return math.tan(angle) / 1.05
 
-print(compliance(1))
-print(compliance(2))
-print(compliance(3))
+compl = []
+
+for i in range(len(array)-1):
+    compl.append(compliance(i,array))
+
+print(compl)
+
