e87f766fa8fa1b1687e21363ee4de6b0b4a10e46
Eris845 <jonathan.halter@outlook.com>
2025-03-10 10:08:04 +0100

    Added imports

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-#CCM -- Daniel
-#ASTM_D5045 -- Teo
-#ASTM_E399 -- Mathijs
-#Area -- Mathijs
+import numpy as np
+from matplotlib import pyplot as plt
+#fracture toughness calculation methods
+import AreaMethod as area
+import ASTM_D5045 as d5045
+import ASTM_E399 as e399
+import ComplianceCalibrationMethod as ccm
+import ModifiedCCM as modccm
+
