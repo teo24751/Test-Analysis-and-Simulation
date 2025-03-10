5656d2ec20aa9bb11ac035c91128881a0b69e369
Eris845 <jonathan.halter@outlook.com>
2025-03-10 11:50:23 +0100

    Added plotting, moved CSVs
    Added plotting function to readData
    Moved CSVs to data_CSV folder

--- readData.py
+++ readData.py
@@ -1,11 +1,12 @@
 import csv
 import numpy as np
+from matplotlib import pyplot as plt
 
 #use the load_displacement_curve function to get a 2D numpy array
 # argument is the sample number (1-3)
 
 def readLD(sampleNo,colNo):
-    filename = "S"+str(sampleNo)+"_load_displ_data.csv"
+    filename = "data_CSV/S"+str(sampleNo)+"_load_displ_data.csv"
     datList = []
     with open(filename, newline='') as csvfile:
         rawDat = csv.reader(csvfile, delimiter=' ', quotechar='|')
@@ -19,6 +20,25 @@ def load_displacement_curve(sampleNo):
     LD = np.stack((readLD(sampleNo,5),readLD(sampleNo,6)), axis=1)
     return LD
 
+def plot_load_displacement_curve(sampleNo):
+    curve = load_displacement_curve(sampleNo)
+    plt.plot(curve[:,1],curve[:,0])
+    plt.title("Load-displacement curve, sample "+str(sampleNo))
+    plt.xlabel("Displacement [mm]") 
+    plt.ylabel("Load [N]")
+    plt.show()
+
+def readCrack(sampleNo, colNo):
+    filename = "data_CSV/S"+str(sampleNo)+"_crack_length_data.csv"
+    datList = []
+    with open(filename, newline='') as csvfile:
+        rawDat = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        next(rawDat)
+        for i in rawDat:
+            x = float((i[-1]).split(",")[colNo])
+            datList.append(x)
+    return np.array(datList)
+
 #for testing
 if __name__ == "__main__":
-    print(load_displacement_curve(2))
+    plot_load_displacement_curve(3)
