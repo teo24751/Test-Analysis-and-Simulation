b304c95c98c837136cd1de19da6f3d79fc33cafc
MineMathijs <mathijs.wijkmans@gmail.com>
2025-03-13 10:33:16 +0100

    vghdsjb

--- ASTM_E399.py
+++ ASTM_E399.py
@@ -3,8 +3,8 @@ import DeterminingCompliance
 import readData
 
 
-def K_IC_func(P_c, aw, t=8, w=70):
-    return (P_c * faw_func(aw)) / (t * np.sqrt(w))
+def K_IC_func(P_c, a, t=8, w=70):
+    return (P_c * faw_func(a/w)) / (t * np.sqrt(w))
 
 def faw_func(aw):
     return ((2 + aw) / ((1 - aw) ** (1.5))) * ((0.866) + (4.64 * aw) - (13.32 * (aw ** 2)) + (14.72 * (aw ** 3)) + (5.6 * (aw ** 4)))
@@ -12,9 +12,9 @@ def faw_func(aw):
 def G_IC_lam(K_IC, E_x, E_y, G_xy, v_xy):
     return ((K_IC ** 2) / (np.sqrt(2 * E_x * E_y))) * np.sqrt(np.sqrt(E_x / E_y) + (E_x / (E_y * G_xy)) - v_xy)
 
-def fracture_toughness():
-    P_c = DeterminingCompliance.compliance()
-    return K_IC_func()
+def fracture_toughness(a,):
+    P_c = ...
+    return K_IC_func(P_c, a)
 
 if __name__ == "__main__":
     print(fracture_toughness())
