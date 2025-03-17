8e0e2656381b1928efdfaa6e085c46364cd80245
teo24751 <teo.cokonaj@gmail.com>
2025-03-17 10:27:12 +0100

    ASTM

--- ASTM_D5045.py
+++ ASTM_D5045.py
@@ -26,69 +26,34 @@ def G_Q(thickness,width,loads,displacements,x):
 
 #Initial values
 
+width=1 #to be modified
+thickness=1 #to be modified
+crack_length=1 #to be modified
 
-sample_number=3
-width=1
-thickness=1
-loads=list(readData.load_displacement_curve(sample_number)[:,0])
-displacements=list(readData.load_displacement_curve(sample_number)[:,1])
-frames=list(readData.load_displacement_curve(sample_number)[:,2])
-crack_tip_length=list(readData.crack_curve(sample_number)[:,0])
-crack_tip_to_edge_length=list(readData.crack_curve(sample_number)[:,1])
-y_displacement=list(readData.crack_curve(sample_number)[:,2]) #twice the displacement 
+for i in range(1,4):
+    sample_number=i
+    loads=list(readData.load_displacement_curve(sample_number)[:,0])
+    displacements=list(readData.load_displacement_curve(sample_number)[:,1])
+    frames=list(readData.load_displacement_curve(sample_number)[:,2])
+    crack_tip_length=list(readData.crack_curve(sample_number)[:,0])
+    crack_tip_to_edge_length=list(readData.crack_curve(sample_number)[:,1])
+    y_displacement=list(readData.crack_curve(sample_number)[:,2]) #twice the displacement 
 
-#crack_lengths=np.array(readData.load_displacement_curve(sample_number)[:,2])
-#x=crack_lengths/width
+    maximum_load=max(loads)
+    displacement_at_max_load=displacements[loads.index(maximum_load)]
 
+    intersection_load,intersection_displacement,original_intersection_load,original_intersection_displacement=P_C_linearized(displacements,loads)
 
-#Linearization of the load-displacement curve
-#Only a part of the data falls on the linear part of the curve - LS regression
-#error_initial=1.1
-#error_final=1.1
-#initial_linear_index=18
-#final_linear_index=40
+    if maximum_load/intersection_load>1.1:
+        print("Test ",sample_number, " is invalid!")
 
-#actual_length=len(loads[initial_linear_index:final_linear_index])
-#print(max(loads))
-
-#sums_of_residuals=[1000,1000]
-
-#S=10000
-#while S>sums_of_residuals[-2] and counter<1000:
-    #counter+=1
-
-    #linear_part_loads=loads[initial_linear_index:final_linear_index]
-    #linear_part_displacements=displacements[initial_linear_index:final_linear_index]
-    #matrix=np.matrix([[final_linear_index-initial_linear_index+1,np.sum(linear_part_displacements)],[np.sum(linear_part_displacements),np.sum((np.array(linear_part_displacements)**2))]])
-    #vector_values=np.matrix([[np.sum(linear_part_loads)],[np.sum(np.array(linear_part_loads)*np.array(linear_part_displacements))]])
-    #line_parameters=np.dot(np.linalg.inv(matrix),vector_values)
-    #linearized_loads=list((np.array(linear_part_displacements))*(line_parameters[1,0])+np.array(line_parameters[0,0]))
-    #linearized_displacements=list(linear_part_displacements)
-    #error_initial=abs(linearized_loads[0]-loads[initial_linear_index])/loads[initial_linear_index]
-    #error_final=abs((linearized_loads[actual_length-1]-loads[final_linear_index]))/loads[final_linear_index]
-
-    #Relative sum of residuals
-    #S=0
-    #for i in range(len(linear_part_loads)):
-     #   S+=(linear_part_loads[i]-linearized_loads[i])
-    #S=S/len(linear_part_loads)
+    elif displacement_at_max_load>original_intersection_displacement and displacement_at_max_load<intersection_displacement:
+        P_Q=maximum_load
     
-
-    #if S>sums_of_residuals[-1]:
-      #  initial_linear_index+=1
-      #  final_linear_index-=1
-       # sums_of_residuals.append(S)
-    #else:
-        #break
-    #if error_initial>2.0*10**(-1):
-    #    
-    #    actual_length-=1
-    #if error_final>2.0*10**(-1): #Currently, the error is less than 5%
-    #    
-    #    actual_length-=1
+    else:
+        P_Q=intersection_load
 
 
-print(P_C_linearized.intersection_load(displacements,loads))
 
 
 
