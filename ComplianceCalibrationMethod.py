f84b917ef440828bc761ac31f0ec16628af8b5a0
Daniël <danielbezaan@gmail.com>
2025-03-17 10:18:18 +0100

    3wedf

--- ComplianceCalibrationMethod.py
+++ ComplianceCalibrationMethod.py
@@ -2,35 +2,59 @@ import numpy as np
 import math
 import scipy as sp
 import matplotlib.pyplot as plt
-
+import readData as rd
 #from ASTM_D5045 import crack_lengths
 from DeterminingCompliance import calculate_compliance
-import readData as rd
+
 
 P_crit = 1000 # Newton
 thickness = 0.008 # meter
-compliance = calculate_compliance()
+compliance_init = calculate_compliance()
+compliance = []
 def func(a,alpha,beta,chi):
     return (alpha * a + beta)**chi
-crack_lengths = np.arange(0.0, len(compliance), 1)
+crack_lengths = []
 #xdata = [0,1,2,3,4,5,6,7,8,10]
 #ydata = [0,1,2,3,4,5,6,7,8,10]
+
+amazing = rd.crack_curve(1)
+
+print(amazing)
+
+for i in range(len(amazing)):
+    crack_lengths.append(amazing[i][0])
+
+for i in range(20, len(amazing)*5+20, 5):
+    compliance.append(compliance_init[i])
+
+
+#print(crack_lengths)
+#print(compliance)
+
+#print(len(compliance), len(crack_lengths))
 
 #print(len(xdata), len(ydata))
 #epic = sp.optimize.curve_fit(func, xdata, ydata, maxfev = 1000)
 
-epic = sp.optimize.curve_fit(func, crack_lengths, compliance, maxfev = 1000)
+epic = sp.optimize.curve_fit(func, crack_lengths, compliance, maxfev = 5000)
 
 #print(epic)
-t = np.arange(0.01, max(crack_lengths)+1, 0.02)
-plt.plot(crack_lengths,compliance)
+t = np.arange(0.000, max(crack_lengths)+0.001, 0.001)
+
 
 alpha = epic[0][0]
 beta = epic[0][1]
 chi = epic[0][2]
-#plt.plot(t, (alpha * t + beta)**chi, 'r--')
+
+#plt.subplot(2, 1, 1)
+plt.plot(crack_lengths,compliance)
+#plt.subplot(2,1,2)
+plt.plot(t, (alpha * t + beta)**chi, 'r--')
+plt.title('Crack-length - Compliance curve')
+plt.xlabel('Crack length')
+plt.ylabel('Compliance')
 plt.show()
-a = 0.02
+
 
 alpha = float(alpha)
 beta = float(beta)
@@ -44,7 +68,7 @@ def G_IC(a, P_crit, alpha, beta, chi, thickness):
 G_IC_list = []
 
 for i in range(len(compliance)):
-    G_IC_list.append(G_IC(crack_lengths[i]/1000, P_crit, alpha, beta, chi, thickness))
+    G_IC_list.append(G_IC(crack_lengths[i], P_crit, alpha, beta, chi, thickness))
 
 
 
@@ -70,8 +94,8 @@ for i in range(len(a_effective)):
 #print('Toughness:', G_IC_mod)
 
 #plt.plot(crack_lengths/1000, G_IC_mod)
-plt.plot(crack_lengths/1000, G_IC_list)
+plt.plot(crack_lengths, G_IC_list)
+plt.title('Crack-length - Energy release rate curve')
+plt.xlabel('Crack length')
+plt.ylabel('Energy release rate')
 plt.show()
-
-for i in range(0,500,10):
-    print(G_IC_list[i])
