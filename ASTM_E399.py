a50664103e7c7717b6c6d01be03efdce5a917755
MineMathijs <mathijs.wijkmans@gmail.com>
2025-03-24 10:22:57 +0100

    Updated ASTM_D399

--- ASTM_E399.py
+++ ASTM_E399.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 import P_C_linearized
 import readData
 
-def K_IC_func(P_c, a, t=0.008, w=0.070):
+def K_IC_func(P_c, a, t=8e-3, w=0.070):
     return (P_c * faw_func(a/w)) / (t * np.sqrt(w))
 
 
@@ -25,32 +25,40 @@ def fracture_toughness(data, crack_curve, E=614e06, v=0.3):
     max_load = max(load)
     displacement_at_max_load = displacement[load.index(max_load)]
     crack_tip = crack_curve[:,0]
-    crack_length = crack_tip[load.index(max_load)]
+    crack_length = crack_tip[load.index(max_load)/5]
+    
+    load_new = []
+    displacement_new = []
+    for i in range(20,451,5):
+        load_new.append(load[i])
+        displacement_new.append(displacement[i])
 
-    intersection_load,intersection_displacement,original_intersection_load,original_intersection_displacement = P_C_linearized.intersection_load(displacement,load)
-    displacement_at_max_load=displacement[load.index(max_load)]
+    # intersection_load,intersection_displacement,_,original_intersection_displacement = P_C_linearized.intersection_load(displacement,load)
+    # displacement_at_max_load=displacement[load.index(max_load)]
 
-    if displacement_at_max_load > original_intersection_displacement and displacement_at_max_load < intersection_displacement:
-        print("Max load")
-        P_Q = max_load
-    else:
-        print("Intersection load")
-        P_Q = intersection_load
+    # if displacement_at_max_load > original_intersection_displacement and displacement_at_max_load < intersection_displacement:
+    #     print("Max load")
+    #     P_Q = max_load
+    # else:
+    #     print("Intersection load")
+    #     P_Q = intersection_load
 
-    print(f"P_Q: {P_Q}")
+    # print(f"P_Q: {P_Q}")
 
-    K_IC = K_IC_func(P_Q, crack_length)
+    # K_IC = K_IC_func(P_Q, crack_length)
+    K_IC = K_IC_func(load_new, crack_tip)
     G_IC = G_IC_lam(K_IC, E, v)
 
     return K_IC, G_IC
 
 
 if __name__ == "__main__":
-    sample = 1
+    sample = 3
     data = readData.load_displacement_curve(sample)
     crack_curve = readData.crack_curve(sample)
     K_IC, G_IC = fracture_toughness(data, crack_curve)
-    print(f"fracture tougness: {K_IC} Pa m^0.5")
-    print(f"energy release rate: {G_IC} J/m^2")
-    print(f"E = { (K_IC ** 2) / G_IC }")
+    print(K_IC, G_IC)
+    # print(f"fracture tougness: {K_IC} Pa m^0.5")
+    # print(f"energy release rate: {G_IC} J/m^2")
+    # print(f"E = { (K_IC ** 2) / G_IC }")
     
