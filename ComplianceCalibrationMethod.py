7febe8362cf98ab6da0069f65d820e737fe4d045
Pau Tarragona <140154617+paut05@users.noreply.github.com>
2025-03-27 10:48:03 +0100

    hello

--- ComplianceCalibrationMethod.py
+++ ComplianceCalibrationMethod.py
@@ -125,7 +125,7 @@ crit_K_IC = fracture_toughness(crit_G_IC, E, 0.3)
 #print(f"Critical crack length is {critical_crack_length} m")
 #print(f"Critical G_IC: {crit_G_IC} J/m^2. Critical K_IC: {crit_K_IC/(10**6)} MPam^0.5")
 
-def CCM():
+def fracture_toughnesses():
     return K_ICs
 
 '''
