b271d96bfd54e1ea51f4083a7c6b0fef2a7eb138
Eris845 <jonathan.halter@outlook.com>
2025-03-27 11:10:40 +0100

    e

--- AreaMethod.py
+++ AreaMethod.py
@@ -21,7 +21,7 @@ def G_IC(short_data, t=0.008, v = 0.3):#Calculates the energy release rate G_IC
     #print(gic)
     return gic
 
-def fracture_toughness(sampleNo, E = 614e6, v=0.3):
+def fracture_toughnesses(sampleNo, E = 614e6, v=0.3):
     short_data = readData.data_short(sampleNo)
     ePrime = E/(1-v*v)
     gic = G_IC(short_data)
@@ -37,7 +37,7 @@ def plot_gic(short_data):
     ax1 = plt.subplot(121)
     ax2 = plt.subplot(122)
     gicdat = G_IC(short_data)
-    kicdat = fracture_toughness(short_data)
+    kicdat = fracture_toughnesses(short_data)
     ax1.plot(gicdat[:,0], gicdat[:,1])
     ax2.plot(kicdat[:,0], kicdat[:,1])
     plt.show()
