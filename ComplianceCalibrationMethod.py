9a0343caec419899626f5eb3ef4b9a2ba842a2a0
Pau Tarragona <140154617+paut05@users.noreply.github.com>
2025-03-27 11:16:55 +0100

    asdf
    asdf

--- ComplianceCalibrationMethod.py
+++ ComplianceCalibrationMethod.py
@@ -73,13 +73,13 @@ def fracture_toughnesses(sample_number):
     #print(f"alpha: {alpha}, beta: {beta}, chi: {chi}")
 
     # Plots the data and the fitted model
-    plt.plot(crack_lengths,compliance, label='Data', linewidth=2, color="gray")
-    plt.plot(t, (alpha * t + beta)**chi, 'r--', label='Fitted model')
-    plt.title('Crack-length - Compliance curve')
-    plt.legend()
-    plt.xlabel('Crack length')
-    plt.ylabel('Compliance')
-    plt.show()
+    ###plt.plot(crack_lengths,compliance, label='Data', linewidth=2, color="gray")
+    ###plt.plot(t, (alpha * t + beta)**chi, 'r--', label='Fitted model')
+    ###plt.title('Crack-length - Compliance curve')
+    ###plt.legend()
+    ###plt.xlabel('Crack length')
+    ###plt.ylabel('Compliance')
+    ###plt.show()
 
     def derivative(a,alpha,beta,chi,h):
         return (func(a+h, alpha, beta, chi)-func(a,alpha,beta,chi))/(h)
@@ -111,11 +111,11 @@ def fracture_toughnesses(sample_number):
     for i in range(len(K_ICs)):
         iter.append(i)
 
-    plt.plot(iter, K_ICs)
-    plt.title(f'Crack-length - Energy release rate curve')
-    plt.xlabel('Crack length [m]')
-    plt.ylabel('Energy release rate [J/m^2]')
-    plt.show()
+    ###plt.plot(iter, K_ICs)
+    ###plt.title(f'Crack-length - Energy release rate curve')
+    ###plt.xlabel('Crack length [m]')
+    ###plt.ylabel('Energy release rate [J/m^2]')
+    ###plt.show()
 
 
     # CRITICAL G_IC AND K_IC VALUES
